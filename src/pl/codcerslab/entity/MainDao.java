32ed3f9db9bee70d7934ca1bf31127cc2b40ca0d
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-06-29 14:36:06 +0200

    I tested UserDao

--- src/pl/codcerslab/entity/MainDao.java
+++ src/pl/codcerslab/entity/MainDao.java
@@ -1,4 +1,15 @@
 package pl.codcerslab.entity;
 
 public class MainDao {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setEmail("machniom@gmail.com");
+        user.setUserName("Mach");
+        user.setPassword("1qaz");
+        UserDao userDao = new UserDao();
+        userDao.create(user);
+
+
+    }
 }
