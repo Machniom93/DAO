44d835c0d102254bc4b93063345dce551bfe616a
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-06-30 21:22:59 +0200

    I finished UserDao methods

--- src/pl/codcerslab/entity/UserDao.java
+++ src/pl/codcerslab/entity/UserDao.java
@@ -4,13 +4,15 @@ import pl.codcerslab.auxiliary.BCrypt;
 import pl.codcerslab.auxiliary.DbUtil;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class UserDao {
 
     private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-    private static final String READ_USER_QUERY = "SELECT username, email, password FROM users WHERE id = ?";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
     private static final String UPDATE_USER_QUERY = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?;";
     private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
+    private static final String FIND_ALL_USER_QUERY = "SELECT * FROM users";
 
     public User create(User user) {
        try (Connection conn = DbUtil.getConnection()) {
@@ -44,13 +46,64 @@ public class UserDao {
                 user.setUserName(resultSet.getString("username"));
                 user.setEmail(resultSet.getString("email"));
                 user.setPassword(resultSet.getString("password"));
+                user.setId(resultSet.getInt("id"));
+            } else {
+                return null;
             }
             return user;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
+    }
 
+    public void update(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_USER_QUERY);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int userId) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USER_QUERY);
+            preparedStatement.setInt(1, userId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAll() {
+        try (Connection connection = DbUtil.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(FIND_ALL_USER_QUERY);
+            User[] users = new User[0];
+            while (resultSet.next()) {
+                User user = new User();
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                user.setId(resultSet.getInt("id"));
+                users = addToArray(user,users);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
+        tmpUsers[users.length] = u;
+        return tmpUsers;
     }
 
 }
